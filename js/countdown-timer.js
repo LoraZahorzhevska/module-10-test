199abdbc3fc1b0d21931fd0ad94b7d1610f7980d
LoraZahorzhevska <Zagorzhevskaya@gmail.com>
2021-04-14 01:03:02 +0300

    добавляет таймер обратного отсчета

---
+++ js/countdown-timer.js
@@ -0,0 +1,55 @@
+const refs = {
+  days: document.querySelector('.value[data-value="days"]'),
+  hours: document.querySelector('.value[data-value="hours"]'),
+  mins: document.querySelector('.value[data-value="mins"]'),
+  secs: document.querySelector('.value[data-value="secs"]'),
+  clockFace: document.getElementById("timer-1"),
+};
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = this.targetDate - currentTime;
+    const time = this.getTimeComponents(deltaTime);
+    this.updateClockface(time);
+    this.timeFinish(deltaTime);
+  }, 1000);
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  updateClockface({ days, hours, mins, secs }) {
+    refs.clockFace.textContent = `${days}:${hours}:${mins}:${secs}`;
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  timeFinish(deltaTime) {
+    if (deltaTime < 0) {
+      clearInterval(this.intervalId);
+      const eventTimer = document.querySelector(".event-description");
+      eventTimer.remove();
+    }
+    return;
+  }
+}
+
+new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("April 24, 2021"),
+});
